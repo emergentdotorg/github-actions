bbac610fc80132cb5dd8bd8d868c7db91b24d080
Patrick Woodworth <patrick@woodworth.org>
2025-10-06 23:22:18 -0700

    Add maven-deploy action and workflow

---
+++ maven-deploy/include.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+SCRIPTDIR="$(unset CDPATH && cd "`dirname "$0"`" && pwd)"
+GITHUB_ACTION_PATH="${GITHUB_ACTION_PATH:-${SCRIPTDIR}}"
+
+catIfExists() {
+  if [ -f "$1" ]; then
+    cat "$1"
+  else
+    echo "WARNING: File $1 does not exist"
+  fi
+}
+
+calcJavaVers() {
+  jdkver="$1"
+  prefix="$2"
+  for ver in 8 11 17 21 ; do
+    if [[ $ver -le $jdkver ]]; then
+      printf '%s%s\n' "$prefix" "$ver"
+    fi
+  done
+}
+
+testCalcJavaVers() {
+  JDK_VER="17"
+  JAVA_SDK_VERSIONS="$( calcJavaVers $JDK_VER )"
+  MVN_TOOLCHAIN_IDS="$( calcJavaVers $JDK_VER 'temurin_' )"
+  echo JAVA_SDK_VERSIONS="${JAVA_SDK_VERSIONS}"
+  echo MVN_TOOLCHAIN_IDS="${MVN_TOOLCHAIN_IDS}"
+}
+
+#"$@"
